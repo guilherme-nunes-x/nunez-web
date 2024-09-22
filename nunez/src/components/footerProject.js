ca3bf6007d26daa88edd91ec458d8b23f21f44a0
guilherme-nunes-x <140085055+guilherme-nunes-x@users.noreply.github.com>
2024-09-22 00:26:18 -0300

    Besteiras

--- nunez/src/components/footerProject.js
+++ nunez/src/components/footerProject.js
@@ -1,52 +1,56 @@
 import "../styles/footer/footer.css";
 function FooterProject() {
   return (
-    <div className="footer">
-      <div className="footer-group">
-        <h2>Serviços</h2>
-        <ul>
-          <li> Consultoria imobiliária </li>
-          <li>avaliação de imóveis</li>
-          <li>gestão de propriedades.</li>
-        </ul>
-      </div>
-      <div className="footer-group">
-        <h2>Políticas de Privacidade</h2>
-        <p>
-          Comprometemo-nos a proteger a sua privacidade<br/> e garantir a segurança
-          dos seus dados.
-        </p>
-      </div>
-      <div className="footer-group">
-        <h2>Termos de Uso</h2>
-        <p>
-          Os serviços oferecidos estão sujeitos aos<br/> nossos termos e condições.
-          <br/>Consulte-nos para mais detalhes.
-        </p>
-      </div>
-      <div className="footer-group">
-        <h2>Desenvolvedores</h2>
-        <ul>
+    <>
+      <div className="footer">
+        <div className="footer-group">
+          <h2>Serviços</h2>
+          <ul>
+            <li> Consultoria imobiliária </li>
+            <li>avaliação de imóveis</li>
+            <li>gestão de propriedades.</li>
+          </ul>
+        </div>
+        <div className="footer-group">
+          <h2>Políticas de Privacidade</h2>
+          <p>
+            Comprometemo-nos a proteger a sua privacidade
+            <br /> e garantir a segurança dos seus dados.
+          </p>
+        </div>
+        <div className="footer-group">
+          <h2>Termos de Uso</h2>
+          <p>
+            Os serviços oferecidos estão sujeitos aos
+            <br /> nossos termos e condições.
+            <br />
+            Consulte-nos para mais detalhes.
+          </p>
+        </div>
+        <div className="footer-group">
+          <h2>Desenvolvedores</h2>
+          <ul>
             <li>Guilherme Nunes Silva</li>
             <li>Carlos Daniel Pereira</li>
             <li>Carlos Andre Santos</li>
-        </ul>
+          </ul>
+        </div>
+        <div className="footer-group">
+          <h2>Contatos</h2>
+          <p>Email: contato@nunezimobiliaria.com</p>
+          <p>Telefone: (11) 98765-4321</p>
+        </div>
+        <div className="footer-group">
+          <h2>Redes Sociais:</h2>
+          <p>
+            <a href="#">Facebook</a>
+          </p>
+          <p>
+            <a href="#">Instagram</a>
+          </p>
+        </div>
       </div>
-      <div className="footer-group">
-        <h2>Contatos</h2>
-        <p>Email: contato@nunezimobiliaria.com</p>
-        <p>Telefone: (11) 98765-4321</p>
-      </div>
-      <div className="footer-group">
-        <h2>Redes Sociais:</h2>
-        <p>
-          <a href="#">Facebook</a>
-        </p>
-        <p>
-          <a href="#">Instagram</a>
-        </p>
-      </div>
-    </div>
+    </>
   );
 }
 export default FooterProject;
--- nunez/src/components/navbarProject.js
+++ nunez/src/components/navbarProject.js
@@ -1,26 +1,30 @@
-import logo from './logo-limpa-golden.png';
+import logo from "./logo-limpa-golden.png";
 import "../styles/navbar/navbar.css";
 function NavbaProject() {
   return (
-    <nav className="navbar">
-      <div className="logo">
-        <a href="app.js"><img src={logo} alt="Logo" /></a>
-      </div>
-      <ul className="nav-links">
-        <li>
-          <a href="#">Propriedades</a>
-        </li>
-        <li>
-          <a href="#">Serviços</a>
-        </li>
-        <li>
-          <a href="#">Sobre Nós</a>
-        </li>
-      </ul>
-      <a href="#" className="cta-button">
-        Agende uma Visita
-      </a>
-    </nav>
+    <>
+      <nav className="navbar">
+        <div className="logo">
+          <a href="app.js">
+            <img src={logo} alt="Logo" />
+          </a>
+        </div>
+        <ul className="nav-links">
+          <li>
+            <a href="#">Propriedades</a>
+          </li>
+          <li>
+            <a href="#">Serviços</a>
+          </li>
+          <li>
+            <a href="#">Sobre Nós</a>
+          </li>
+        </ul>
+        <a href="#" className="cta-button">
+          Agende uma Visita
+        </a>
+      </nav>
+    </>
   );
 }
 export default NavbaProject;
